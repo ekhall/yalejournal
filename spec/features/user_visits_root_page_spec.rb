d15ccf96a9c3c5659dd8ab36b4f42183394816d5
E. Kevin Hall <hall@absinthe.org>
2013-11-04 12:09:08 -0500

    Add Invalid User feature

--- spec/features/user_visits_root_page_spec.rb
+++ spec/features/user_visits_root_page_spec.rb
@@ -11,4 +11,9 @@ feature 'User accesses homepage' do
     sign_in_user
     expect(page).to have_content('Sign Out')
   end
+
+  scenario 'and attempts to log in with invalid credentials' do
+    sign_in_with('invalid@example.com', 'invalid_password')
+    expect(page).to have_content('Invalid email/password')
+  end  
 end
